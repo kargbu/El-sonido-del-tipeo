6118d083c2a2630e4dfd9bb40f027fe3c1df2256
kargbu <kargb@ciencias.unam.mx>
2024-11-26 21:01:37 -0300

    Subiendo la tarea de la clase 11

--- clase_11/Tarea_ADA/ejercicio_2.js
+++ clase_11/Tarea_ADA/ejercicio_2.js
@@ -6,3 +6,18 @@ Dependiendo del color ingresado, el progrma deberá imprimir un mensaje con la a
 Si el usuario ingresa un color inválido, el programa debe mostrar un
 mensaje: "Color no reconocido, ingresa rojo, amarilo o verde."
 Utiliza un switch para resolver este ejercicio.*/
+const prompt = require('prompt-sync')();
+
+let color_usuario = prompt('De los colores de un semáforo, ingresa un color: ');
+let color = color_usuario.toLowerCase();
+
+switch (color) {
+    case color ==  'verde':
+        console.log('Avanza con seguridad');
+        break;
+    case color == 'amarillo':
+        console.log('Precaución, prepárate para avanzar');
+        break;
+    default:
+        console.log('Alto, no puedes avanzar');
+}
