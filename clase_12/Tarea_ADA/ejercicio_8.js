aa39a9ea50a1489ac256d74b60fcd93d23a0bef1
kargbu <kargb@ciencias.unam.mx>
2024-11-29 10:01:18 -0300

    tarea clase 12

---
+++ clase_12/Tarea_ADA/ejercicio_8.js
@@ -0,0 +1,24 @@
+/* Para este ejercicio vamos a generar dos funciones:
+a) Una va a sumar los valores en la diagonal marcada en rojo.
+b) La otra va a marcar los valores de la diagonal marcada en verde.
+Ambas funciones deben devolver un resultado único. Rojo: 505
+Verde 505. */
+
+// Matriz de 10X10 cuyas entradas comiencen en 1 y termine en 100
+let matriz_10x10 = [];
+let valor = 1;
+
+for (let i = 0; i < 10; i++) {
+    let filas = [];
+    for (let j = 0; j < 10; j++) {
+        filas.push(valor); // Agregar el valor actual a la fila
+        valor++;           // Incrementar el valor
+    }
+    matriz_10x10.push(filas); // Agregar la fila completa a la matriz
+}
+
+console.log(matriz_10x10);
+
+// Sumar la diagonal
+let suma_diag = 1
+
