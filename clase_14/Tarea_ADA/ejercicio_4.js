2e2344cd032d3789f40a9b6cb59c29b170e57ef2
K G B <kgb@MacBookdeKGB.fibertel.com.ar>
2024-12-15 21:31:07 -0300

    tarea 4 de la clase 14

---
+++ clase_14/Tarea_ADA/ejercicio_4.js
@@ -0,0 +1,16 @@
+/*
+Calcular descuento dada una cantidad.
+1. Solicita al usuario la cantidad del producto y el precio unitario.
+2. Usa la función flecha para calcular el total considerando el descuento.
+*/
+const prompt = require('prompt-sync')();
+
+let precio_unitario = parseInt(prompt('Ingresa el precio del producto: '));
+let cantidad = parseInt(prompt('Ingresa la cantidad de productos que llevas: '));
+let descuento = parseInt(prompt('Ingresa el porcentaje de descuento:  '));
+
+let des = () => {
+
+}
+
+
