14a4f9f23bbcc63b91b85a1c9b283ee5876fe1fc
K G B <kgb@MacBook-de-K-G-B.local>
2024-12-23 13:51:00 -0300

    tarea 6 de la clase 14

--- clase_14/Tarea_ADA/ejercicio_6.js
+++ clase_14/Tarea_ADA/ejercicio_6.js
@@ -1,3 +1,22 @@
 /*
-Objetivo
+Objetos
 */
+const prompt = require('prompt-sync')();
+
+let horas_entrenamiento = parseFloat(prompt('Ingresa el número de horas de entrenamiento en la semana: '));
+let deportista = {
+    nombre        : prompt('Ingresa el nombre del deportista: '),
+    energia       : parseFloat(prompt('¿Cuánta energía hay en la semana: ')),
+    experiencia   : parseFloat(prompt('¿Cuánta experiencia tienes? ')),
+    entrenamiento : function entrenar_horas(horas) {
+        this.energia -= horas * 5; // Ejemplo de reducción de energía por horas de entrenamiento
+        this.experiencia += horas * 2; // Ejemplo de aumento de experiencia por horas de entrenamiento
+    }
+};
+
+// Ejemplo de uso de la función de entrenamiento
+deportista.entrenamiento(horas_entrenamiento);
+console.log(`Nombre: ${deportista.nombre}`);
+console.log(`Energía: ${deportista.energia}`);
+console.log(`Experiencia: ${deportista.experiencia}`);
+
