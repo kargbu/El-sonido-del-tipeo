d71e54e351c8674ca9544e63a0f0290442f2bae8
K G B <kgb@MacBook-de-K-G-B.local>
2024-12-31 20:56:33 -0300

    proyecto final

---
+++ proyecto_final/libros_datos.js
@@ -0,0 +1,88 @@
+/* Estructura de datos */
+
+const libros = [
+    {
+        id: 1,
+        titulo: "Balas de plata",
+        autor: "Elmer Mendoza",
+        año: 2008,
+        genero: "Novela negra",
+        disponible: true
+
+    },
+    {
+        id: 2,
+        titulo: "El año del desierto",
+        autor: "Pedro Mairal",
+        año: 2005,
+        genero: "Novela",
+        disponible: true
+    },
+    {
+        id: 3,
+        titulo: "Balún Canán",
+        autor: "Rosario Castellanos",
+        año: 1957,
+        genero: "Novela",
+        disponible: true
+    },
+    {
+        id: 4,
+        titulo: "La condesa sangrienta",
+        autor: "Alejandra Pizarnik",
+        año: 1971,
+        genero: "Ensayo",
+        disponible: true
+    },
+    {
+        id: 5,
+        titulo: "El infinito en un junco",
+        autor: "Irene Vallejo",
+        año: 2019,
+        genero: "Ensayo",
+        disponible: true
+    },
+    {
+        id: 6,
+        titulo: "Contra los hijos",
+        autor: "Lina Meruane",
+        año: 2014,
+        genero: "Ensayo",
+        disponible: true
+    },
+    {
+        id: 7,
+        titulo: "El complot de los románticos",
+        autor: "Carmen Boullosa",
+        año: 2009,
+        genero: "Novela",
+        disponible: true
+    },
+    {
+        id: 8,
+        titulo: "Los detectives salvajes",
+        autor: "Roberto Bolaño",
+        año: 1998,
+        genero: "Novela",
+        disponible: true
+    },
+    {
+        id: 9,
+        titulo: "Rayuela",
+        autor: "Julio Cortázar",
+        año: 1963,
+        genero: "Novela",
+        disponible: true
+    },
+    {
+        id: 10,
+        titulo: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        año: 1967,
+        genero: "Novela",
+        disponible: false
+    }
+];
+
+console.log(libros);
+
