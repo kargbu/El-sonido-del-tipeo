bb1187d905d6765bded754f020c7f682095316ff
K G B <kgb@MacBook-de-K-G-B.local>
2025-01-05 16:45:49 -0300

    tarea

---
+++ proyecto_final/usuarios_datos.js
@@ -0,0 +1,34 @@
+/* Estructura de datos */
+
+const usuarios = [
+    {
+        id: 1,
+        nombre: 'Francisco Marmolejo',
+        email: 'quicosh@gmail.com',
+        librosPrestados: [1, 9, 10]
+    },
+    {
+        id: 2,
+        nombre: 'Carolina Leschevich',
+        email: 'caro@gmail.com',
+        librosPrestados: [8, 9, 10]
+    },
+    {
+        id: 3,
+        nombre: 'Arturo Sartillo',
+        email: 'artur@gmail.com',
+        librosPrestados: [1, 9] 
+    },
+    {
+        id: 4,
+        nombre: 'Tenoch Sartillo Leschevisch',
+        email: 'tenochevich@gmail.com',
+        librosPrestados: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    {
+        id: 5,
+        nombre: 'Karina García',
+        email: 'kargarciab@ciencias.unam.mx',
+        librosPrestados: []
+    }
+]
