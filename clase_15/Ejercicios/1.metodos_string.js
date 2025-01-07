e88f2e795ba9def1a9ae9382c66434829d60e0ef
K G B <kgb@MacBook-de-K-G-B.local>
2025-01-07 17:24:23 -0300

    tarea de la clase 15

---
+++ clase_15/Ejercicios/1.metodos_string.js
@@ -0,0 +1,5 @@
+let frase = 'Breaking Bad Rules';
+
+console.log(frase.slice(9,12));
+console.log(frase.slice(13));
+console.log(frase.slice(-10));
---
+++ clase_15/Ejercicios/2.propiedades_string.js
@@ -0,0 +1,6 @@
+/* Los métodos y las propiedades son "funciones" que permiten manipular string
+trim quita espacios al principio y el final del string */
+
+let nombreCompleto = ' Karina García Buendía ';
+
+console.log(nombreCompleto.trim());
