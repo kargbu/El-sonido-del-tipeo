bf516212b1e5b50b2c01fada6d0d6261a31ca4b7
K G B <kgb@MacBook-de-K-G-B.local>
2025-01-07 19:55:10 -0300

    clase 15

---
+++ clase_15/Tarea_ADA/ejercicio_4.js
@@ -0,0 +1,7 @@
+/* Convierte una lista de palabras en una frase */
+
+let palabras = ["JavaScript", "es", "divertido."];
+
+let frase = palabras.join(' ');
+
+console.log(frase);
---
+++ clase_15/Tarea_ADA/ejercicio_5.js
@@ -0,0 +1,5 @@
+/* Buscar la posición de un elemento */
+
+let colores = ["Rojo", "Verde", "Azul", "Amarillo"];
+let indice_azul = colores.indexOf('Azul');
+console.log(indice_azul);
