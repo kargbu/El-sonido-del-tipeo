08e7625ab2f6dda90c38a807560dbfe5e3b41f05
K G B <kgb@MacBook-de-K-G-B.local>
2025-01-09 12:02:58 -0600

    clase 16

---
+++ clase_16/Ejercicios/Tarea_ADA/ejercicio_10.js
@@ -0,0 +1,7 @@
+/*
+Construye una frase a partir de una lista de palabras. Si la lista es ['Me', 'gusta', 'aprender', 'JavaScript'],
+escribe una frase completa concatenada.
+*/
+const lista_palabras = ['Me', 'gusta', 'aprender', 'JavaScript'];
+const frase_completa = lista_palabras.join(' ');
+console.log(frase_completa)
